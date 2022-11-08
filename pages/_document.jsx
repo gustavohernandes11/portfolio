09e65b41e18b5909c8d9e3af441700134d8b96e4
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2022-11-08 14:09:31 -0300

    modificado atributo crossOrigin

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -45,7 +45,7 @@ export default class MyDocument extends Document {
                     <link
                         rel="preconnect"
                         href="https://fonts.gstatic.com"
-                        crossOrigin
+                        crossOrigin="true"
                     />
                     <link
                         href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,400;0,500;0,900;1,400&display=swap"
